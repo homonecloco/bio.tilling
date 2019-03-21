534f39052788fde313f2d4c307237287bf30a60d
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2019-03-21 15:18:14 +0000

    Added options to vary the standard deviation for the deletions

--- bin/detect_deletions_simple.r
+++ bin/detect_deletions_simple.r
@@ -1,21 +1,31 @@
 #!/usr/bin/env Rscript
 library("optparse")
-
 options(echo=TRUE) 
-#devtools::install_github("homonecloco/bio.tilling")
-#dirname(sys.frame(0)$ofile)
-#programDir <- dirname(sys.frame(1)$ofile)
-
-#devtools::install_local("/Users/ramirezr/Documents/public_code/bio.tilling")
 require('bio.tilling')
 
 option_list = list(
-  make_option(c("-f", "--coverage_file"), type="character", default=NULL, 
-              help="Coverage file", metavar="character"),
-  make_option(c("-z", "--gzip"), action="store_true", default=FALSE,
-            help="The coverage file is gzipped [default]"),
-  make_option(c("-o", "--out"), type="character", default="./deletions_out", 
-              help="output folder [default= %default]", metavar="character")
+  make_option(c("-f", "--coverage_file"), 
+  	type="character", 
+  	default=NULL, 
+  	help="Coverage file", 
+  	metavar="character"),
+  make_option(c("-s", "--sampleSD"), 
+  	type="double", 
+  	default=0.3, 
+  	help="Maximum Standard Deviation allowed in sample [default= %default]"),
+  make_option(c("-w", "--windowSD"), 
+  	type="double", 
+  	default=0.3,
+  	help="Maximum Standard Deviation allowed in each window [default= %default]"),
+  make_option(c("-z", "--gzip"), 
+  	action="store_true", 
+  	default=FALSE,
+  	help="The coverage file is gzipped [default]"),
+  make_option(c("-o", "--out"), 
+  	type="character", 
+  	default="./deletions_out", 
+  	help="output folder [default= %default]", 
+  	metavar="character")
 ); 
 
 opt_parser = OptionParser(option_list=option_list);
@@ -24,20 +34,16 @@ opt
 
 if (is.null(opt$coverage_file)){
   print_help(opt_parser)
-  stop("At least one argument must be supplied (coverage_file).", call.=FALSE)
+  stop("At least one argument must be supplied (coverage_file).",
+  	call.=FALSE)
 }
-
-
 
 filename<-unlist(opt$coverage_file)
 output_folder<-opt$out
 is_gz <- opt$gzip
 
-
-#cores<-as.integer(args[2])
-
 covs<-readCoverageTable(filename, is_gz=is_gz)
-covs<-filterSamplesPerSD(covs, maxSD=0.3)
+covs<-filterSamplesPerSD(covs, maxSD=opt$sampleSD)
 
 df<-getExonsDF(covs)
 mat<-normalizeCovs(covs, df)
@@ -46,7 +52,7 @@ rm(covs)
 dir.create(output_folder, recursive = TRUE)
 setwd(output_folder)
 
-mat<-filterLowQualityExons(mat, maxSD=0.3)
+mat<-filterLowQualityExons(mat, maxSD=opt$windowSD)
 gc()
 write.csv(mat, file='mat.csv')
 
@@ -58,5 +64,3 @@ write.csv(dels, file="dels.csv")
 
 libSD<-getLibSD(mat)
 write.csv(libSD, file='libSD.csv')
-
-
